53178dcc4fa9d3db5a9985c1bca7b2305e646240
Polina <polpan01@mail.ru>
2021-06-14 08:12:26 +0300

    tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,17 +6,17 @@
 TEST(TrainTest, test1) {
   Train train;
   for (int i = 0; i < 15; i++) { train.add_cage(); }
-  EXPECT_EQ(9, train.find_length());
+  EXPECT_EQ(15, train.find_length());
 }
 
 TEST(TrainTest, test2) {
   Train train;
   for (int i = 0; i < 500; i++) { train.add_cage(); }
-  EXPECT_EQ(1206, train.find_length());
+  EXPECT_EQ(500, train.find_length());
 }
 
 TEST(TrainTest, test3) {
   Train train;
   for (int i = 0; i < 1000; i++) { train.add_cage(); }
-  EXPECT_EQ(10300, train.find_length());
+  EXPECT_EQ(1000, train.find_length());
 }
